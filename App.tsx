d75240df2e4bff2c7b63901a087789f1cd1eda03
Salehi <apple@Apples-MacBook-Air.local>
2024-02-24 22:44:15 +0330

    solving

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './src/navigations';
+import { StatusBar } from "expo-status-bar";
+import Navigation from "./src/navigations";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <Navigation />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
