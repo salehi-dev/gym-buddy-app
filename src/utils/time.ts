1afad5a8ed4602307bb01a5db78a4abe8eba322d
Salehi <apple@Apples-MacBook-Air.local>
2024-02-26 12:15:08 +0330

    Feat: Improve format of sec and min

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -8,5 +8,20 @@ export function secToMin(sec: number): number {
 export function formatSec(sec: number) {
   const _min = Math.floor(sec / 60);
   const _sec = sec % 60;
-  return `${_min} min and ${_sec} sec`;
+
+  const minText = `${_min} min`;
+  const secText = `${_sec} sec`;
+  const connector = "and";
+
+  const sentence = [];
+  if (_min > 0) {
+    sentence.push(minText);
+  }
+  if (_min > 0 && _sec > 0) {
+    sentence.push(connector);
+  }
+  if (_sec > 0) {
+    sentence.push(secText);
+  }
+  return sentence.join(" ");
 }
