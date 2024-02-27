bebc51e10f55794b96efa69de451f4b550688676
Aref Salehi <m.arf.salehi@gmail.com>
2024-02-27 21:52:29 +0330

    Feat: create PressableText

--- src/screens/WorkoutDetailScreen.tsx
+++ src/screens/WorkoutDetailScreen.tsx
@@ -1,11 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigations/Stack";
+import { useWorkoutBySlug } from "../hooks/useWorkoutBySlug";
+import PressableText from "../components/styled/PressableText";
 
 type Props = NativeStackScreenProps<RootStackParamList, "WorkDetail">;
 
 export default function WorkoutDetailScreen({ route }: Props) {
-  return <View style={styles.container}></View>;
+  const workout = useWorkoutBySlug(route.params.slug);
+  if (!workout) {
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>{workout.name}</Text>
+      <PressableText
+        text="Check Sequence"
+        onPress={() => alert("Opening modal")}
+      />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
