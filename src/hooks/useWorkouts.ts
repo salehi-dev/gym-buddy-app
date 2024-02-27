aa3948a67c9f8b01a6ddfa03bbc6df5ba4a23aa1
Aref Salehi <m.arf.salehi@gmail.com>
2024-02-27 12:10:58 +0330

    Implement: useIsFocused

--- src/hooks/useWorkouts.ts
+++ src/hooks/useWorkouts.ts
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+
 import { Workout } from "../types/data";
 import { getWorkouts } from "../storage/workout";
 
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
-
+  const isFocused = useIsFocused();
   useEffect(() => {
     async function getData() {
       const _workouts = await getWorkouts();
       setWorkouts(_workouts);
+      console.log("get data");
     }
-    getData();
-  }, []);
+    if (isFocused) {
+      getData();
+    }
+  }, [isFocused]);
   return workouts;
 };
