d329a0352e4ed18d18ac474b3df25d49342fe518
Aref Salehi <m.arf.salehi@gmail.com>
2024-02-27 18:32:38 +0330

    First check the workout

--- src/screens/WorkoutDetailScreen.tsx
+++ src/screens/WorkoutDetailScreen.tsx
@@ -1,19 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigations/Stack";
-import { useEffect } from "react";
-import { getWorkoutBySlug } from "../storage/workout";
-import { useWorkoutBySlug } from "../hooks/useWorkoutBySlug";
 
 type Props = NativeStackScreenProps<RootStackParamList, "WorkDetail">;
 
 export default function WorkoutDetailScreen({ route }: Props) {
-  const workout = useWorkoutBySlug(route.params.slug);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>{workout?.name}</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 const styles = StyleSheet.create({
   container: {
