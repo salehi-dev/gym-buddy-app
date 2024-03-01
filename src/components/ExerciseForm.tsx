fa7fc951c89b3902a7a597baa354053e420a1f60
Aref Salehi <m.arf.salehi@gmail.com>
2024-03-02 02:13:28 +0330

    Feat: create ExerciseItem component

--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -114,6 +114,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 10,
+    marginBottom: 20,
   },
   formContainer: {
     marginVertical: 8,
--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,9 +1,13 @@
-import { View, StyleSheet } from "react-native";
-import WorkoutForm, { ExerciseFormData } from "../components/ExerciseForm";
-import { SequenceItem, SequenceType } from "../types/data";
+import { useState } from "react";
+import { View, StyleSheet, Text, FlatList, ScrollView } from "react-native";
 import slugify from "slugify";
 
+import WorkoutForm, { ExerciseFormData } from "../components/ExerciseForm";
+import { SequenceItem, SequenceType } from "../types/data";
+import ExerciseItem from "../components/ExerciseItem";
+
 export default function PlannerScreen() {
+  const [seqItems, setSeqItems] = useState<SequenceItem[]>([]);
   const handleFormSubmit = (form: ExerciseFormData) => {
     const sequenceItem: SequenceItem = {
       slug: slugify(`${form.exerciseName} ${Date.now()}`, { lower: true }),
@@ -14,12 +18,20 @@ export default function PlannerScreen() {
     if (form.reps) {
       sequenceItem.reps = form.reps;
     }
-    console.log(sequenceItem);
+    setSeqItems([...seqItems, sequenceItem]);
   };
   return (
-    <View style={styles.container}>
-      <WorkoutForm onSubmit={handleFormSubmit} />
-    </View>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      keyboardDismissMode="on-drag"
+    >
+      <View style={styles.container}>
+        <WorkoutForm onSubmit={handleFormSubmit} />
+        {seqItems.map((item) => (
+          <ExerciseItem item={item} />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
