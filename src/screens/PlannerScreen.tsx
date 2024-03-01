ba9ba98395de0dd91ccd268e2aecc1b3fef0f8d1
Aref Salehi <m.arf.salehi@gmail.com>
2024-03-02 00:38:15 +0330

    use slugify package

--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,11 +1,12 @@
 import { View, StyleSheet } from "react-native";
 import WorkoutForm, { ExerciseFormData } from "../components/ExerciseForm";
 import { SequenceItem, SequenceType } from "../types/data";
+import slugify from "slugify";
 
 export default function PlannerScreen() {
   const handleFormSubmit = (form: ExerciseFormData) => {
     const sequenceItem: SequenceItem = {
-      slug: `${form.exerciseName.split(" ").join("")}${Date.now()}`,
+      slug: slugify(`${form.exerciseName} ${Date.now()}`, { lower: true }),
       name: form.exerciseName,
       duration: Number(form.duration),
       type: form.type as SequenceType,
